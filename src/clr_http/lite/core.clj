16e8c3d57d6c53878c18a6da5cef98173ccc927e
Matthew Molloy <whamtet@gmail.com>
2015-02-21 19:34:11 +0800

    converted core

--- src/clr_http/lite/core.clj
+++ src/clr_http/lite/core.clj
@@ -1,50 +1,45 @@
-(ns clj-http.lite.core
+(ns clr-http.lite.core
   "Core HTTP request/response implementation."
-  (:require [clojure.java.io :as io])
-  (:import (java.io ByteArrayOutputStream InputStream IOException)
-           (java.net URI URL HttpURLConnection)))
+  (:require [clojure.clr.io :as io]
+            [clr-http.lite.util :as util]
+            )
+  (:import
+   System.Net.WebRequest
+   #_(java.io ByteArrayOutputStream InputStream IOException)
+   #_(java.net URI URL HttpURLConnection)))
+
+(defn safe-conj [a b]
+  (if (vector? a)
+    (conj a b)
+    [a b]))
 
 (defn parse-headers
   "Takes a URLConnection and returns a map of names to values.
 
-   If a name appears more than once (like `set-cookie`) then the value
-   will be a vector containing the values in the order they appeared
-   in the headers."
+  If a name appears more than once (like `set-cookie`) then the value
+  will be a vector containing the values in the order they appeared
+  in the headers."
   [conn]
-  (loop [i 1 headers {}]
-    (let [k (.getHeaderFieldKey ^HttpURLConnection conn i)
-          v (.getHeaderField ^HttpURLConnection conn i)]
-      (if k
-        (recur (inc i) (update-in headers [k] conj v))
-        (zipmap (for [k (keys headers)]
-                  (.toLowerCase ^String k))
-                (for [v (vals headers)]
-                  (if (= 1 (count v))
-                    (first v)
-                    (vec v))))))))
+  (let [headers (.Headers conn)]
+    (apply merge-with
+           safe-conj
+           (for [header (.Headers conn)]
+             {header (.Get headers header)}))))
 
 (defn- coerce-body-entity
   "Coerce the http-entity from an HttpResponse to either a byte-array, or a
   stream that closes itself and the connection manager when closed."
   [{:keys [as]} conn]
-  (let [ins (try
-              (.getInputStream ^HttpURLConnection conn)
-              (catch Exception e
-                (.getErrorStream ^HttpURLConnection conn)))]
+  (let [ins (.GetResponseStream conn)]
     (if (or (= :stream as) (nil? ins))
       ins
-      (with-open [ins ^InputStream ins
-                  baos (ByteArrayOutputStream.)]
-        (io/copy ins baos)
-        (.flush baos)
-        (.toByteArray baos)))))
+      (util/to-byte-array ins))))
 
 (defn request
   "Executes the HTTP request corresponding to the given Ring request map and
-   returns the Ring response map corresponding to the resulting HTTP response.
-
-   Note that where Ring uses InputStreams for the request and response bodies,
-   the clj-http uses ByteArrays for the bodies."
+  returns the Ring response map corresponding to the resulting HTTP response.
+  Note that where Ring uses InputStreams for the request and response bodies,
+  the clj-http uses ByteArrays for the bodies."
   [{:keys [request-method scheme server-name server-port uri query-string
            headers content-type character-encoding body socket-timeout
            conn-timeout multipart debug insecure? save-request? follow-redirects] :as req}]
@@ -52,30 +47,42 @@
                       (when server-port (str ":" server-port))
                       uri
                       (when query-string (str "?" query-string)))
-        conn (.openConnection ^URL (URL. http-url))]
+        request (WebRequest/Create http-url)
+        Headers (.Headers request)
+        ]
     (when (and content-type character-encoding)
-      (.setRequestProperty conn "Content-Type" (str content-type
-                                                    "; charset="
-                                                    character-encoding)))
+      (.ContentType request (str content-type
+                                 "; charset="
+                                 character-encoding)))
     (when (and content-type (not character-encoding))
-      (.setRequestProperty conn "Content-Type" content-type))
+      (.ContentType request content-type))
     (doseq [[h v] headers]
-      (.setRequestProperty conn h v))
+      (.Add Headers h v))
     (when (false? follow-redirects)
-      (.setInstanceFollowRedirects ^HttpURLConnection conn false))
-    (.setRequestMethod ^HttpURLConnection conn (.toUpperCase (name request-method)))
-    (when body
-      (.setDoOutput conn true))
+      (.AllowAutoRedirect request false))
+    ;(.Method request (.ToUpper (name request-method)))
+
     (when socket-timeout
-      (.setReadTimeout conn socket-timeout))
-    (.connect conn)
+      (.ReadWriteTimeout request socket-timeout))
     (when body
-      (with-open [out (.getOutputStream conn)]
+      (with-open [out (.GetRequestStream request)]
         (io/copy body out)))
-    (merge {:headers (parse-headers conn)
-            :status (.getResponseCode ^HttpURLConnection conn)
-            :body (when-not (= request-method :head)
-                    (coerce-body-entity req conn))}
-           (when save-request?
-             {:request (assoc (dissoc req :save-request?)
-                         :http-url http-url)}))))
+    (let [
+          response (.GetResponse request)
+          ]
+      (merge {:headers (parse-headers response)
+              :status (.StatusCode response)
+              :body (when-not (= request-method :head)
+                      (coerce-body-entity req response))}
+             (when save-request?
+               {:request (assoc (dissoc req :save-request?)
+                           :http-url http-url)})))))
+
+(defn t []
+  (-> {:scheme "http"
+       :server-name "www.google.com"
+       :request-method "GET"
+       }
+      request
+      (update-in [:body] util/utf8-string)
+      pr-str))
