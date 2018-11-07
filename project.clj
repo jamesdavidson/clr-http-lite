eee0cb5a08179f8e37130e07ef719444f82fe49d
SeniuuS <seniuu.s1@gmail.com>
2018-11-07 22:46:52 +0100

    More generic project.clj with clojureclr path

--- project.clj
+++ project.clj
@@ -2,17 +2,16 @@
   :description "Http client for ClojureCLR based on clj-http-lite."
   :url "https://github.com/whamtet/clr-http-lite/"
   :dependencies [
-                 [org.clojure/clojure "1.6.0"]
-            [ring/ring-jetty-adapter "1.3.2"]
-            [ring/ring-devel "1.3.2"]]
-  :plugins [[lein-clr "0.2.0"]
+                  [org.clojure/clojure "1.6.0"]
+                  [ring/ring-jetty-adapter "1.3.2"]
+                  [ring/ring-devel "1.3.2"]
+                 ]
+  :plugins [
+            [lein-clr "0.2.0"]
             ]
   :clr {
-;        :main-cmd      [:clj-exe "Clojure.Main.exe"]
-        :compile-cmd   [:clj-exe "Clojure.Compile.exe"]
-        :cmd-templates{
-                       :clj-exe   [
-                                   ["Z:\\Downloads\\clojure-clr\\bin\\4.0\\Release" %1] ;latest build
-                                   ]
-                       }}
+        :cmd-templates  {:clj-exe   [[CLJCLR14_40 %1]]}
+        :main-cmd [:clj-exe "Clojure.Main.exe"]
+        :compile-cmd [:clj-exe "Clojure.Compile.exe"]
+        }
   )
